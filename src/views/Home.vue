c3bd345be2bc831f950c2789b61617564ea93a02
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-06-22 19:19:03 +0300

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1 class="home__header">
+      Список контактов
+    </h1>
+    <ContactList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ContactList from '@/components/ContactList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ContactList
+  }
+}
+</script>
